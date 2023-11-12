dfe63b87819032c776193cdd27a52bad12b0f437
BeatrizLdS <beatriz_l.silva@hotmail.com>
2023-11-12 14:09:29 -0300

    :wheelchair: feat: resize Screen and canvas

--- script.js
+++ script.js
@@ -23,6 +23,27 @@ window.addEventListener('load', function() {
     animate();
 });
 
+window.addEventListener('resize', function() {
+    const canvas = document.getElementById('canvas1');
+    const context = canvas.getContext('2d');
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    const player = new Player(canvas.width, canvas.height);
+    const input = new InputHandler();
+    let control = 0;
+
+    function animate() {
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        control++;
+        drawStatustext(context, input, player);
+        player.update(control, input.lastKey);
+        player.draw(context);
+        requestAnimationFrame(animate);
+    };
+    animate();
+});
+
 // const canvas = document.getElementById('canvas1');
 // const context = canvas.getContext('2d');
 
