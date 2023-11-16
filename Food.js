19b9718ac966c9ca990c7824d9ffa70095d5af0b
BeatrizLdS <beatriz_l.silva@hotmail.com>
2023-11-15 21:39:24 -0300

    :construction:

--- Food.js
+++ Food.js
@@ -1,16 +1,36 @@
-class Food {
-    constructor() {
-        this.x = 10;
-        this.y = 10;
-        this.width = 100;
-        this.height = 100;
+export default class Food {
+    constructor(startX, y) {
+        let imageName = this.#getRandomImageName();
+
+        this.image = new Image();
+        this.image.src = './Resources/Foods/'+imageName;
+        this.x = startX;
+        this.originalY = y;
+        this.y = y;
+        this.width = 300;
+        this.height = 300;
+    }
+
+    #getRandomImageName() {
+        var types = ['1', '2'];
+        let indexType = Math.floor(Math.random() * types.length);
+        let type = types[indexType];
+
+        let names = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15'];
+        let indexName = Math.floor(Math.random() * names.length);
+        let name = names[indexName];
+
+        return type+'-'+name+'.png';
     }
 
     update() {
-
+        this.x--;
     }
     
-    draw() {
-
+    draw(context) {
+        context.drawImage(this.image,
+            this.x, this.y,
+            this.width, this.height
+        );  
     }
 }
---
+++ Game.js
@@ -0,0 +1,100 @@
+import Player from './player.js';
+import Layer from './Layer.js';
+import Food from './Food.js';
+
+export default class Game {
+    #control = 0;
+    #timeIntervalForFood = 400;
+    #timeLastFood = 0;
+
+    constructor(gameWidth, gameHeight) {
+        this.gameWidth = gameWidth;
+        this.gameHeight = gameHeight;
+
+        this.groundStatic;
+        this.foods = [];
+        this.layers = [];
+        this.player;
+        this.speed = 0;
+    }
+
+    update(input) {
+        if ( this.#timeLastFood >= this.#timeIntervalForFood ) {
+            this.#addNewEnemy();
+            this.#timeLastFood = 0
+        } else {
+            this.#timeLastFood++;
+        }
+
+        this.#control++;
+
+        this.layers.forEach( layer => {
+            layer.update();
+        })
+
+        this.foods.forEach( food => {
+            food.update();
+        })
+
+        this.player.update(this.#control, input.lastKey);
+    }
+
+    draw(context) { 
+        this.groundStatic.draw(context);
+
+        this.layers.forEach( layer => {
+            layer.draw(context);
+        })
+
+        this.foods.forEach( food => {
+            food.draw(context);
+        })
+
+        this.player.draw(context);
+    }
+
+    createLayers() {
+        let ground = new Layer(
+            this.gameWidth, this.gameHeight,
+            './Resources/Layer/ground.png', this.speed,
+            0, this.gameHeight - 90,
+            this.gameWidth, 90
+        );
+        
+        let groundStatic = new Layer(
+            this.gameWidth, this.gameHeight,
+            './Resources/Layer/ground.png', 0,
+            0, this.gameHeight - 90,
+            this.gameWidth, 90
+        );
+      
+        let sky = new Layer(
+            this.gameWidth, this.gameHeight,
+            './Resources/Layer/sky.png', this.speed / 5,
+            this.gameWidth / 6, 0,
+            1000, 500
+        );
+
+        this.groundStatic = groundStatic;
+        this.layers = [sky, ground];
+    }
+
+    createPlayer(playerName) {
+        this.player = new Player( 
+            this.gameWidth, 
+            this.gameHeight, 
+            90
+        );
+    }
+
+    updateSpeed(newSpeed) {
+        this.speed = newSpeed;
+        this.layers.forEach ( layer => {
+            layer.speed = this.speed;
+        })
+    }
+
+    #addNewEnemy() {
+        this.foods.push(new Food(this.gameWidth, 90));
+    }
+}
